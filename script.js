173af1e59ba212d0186e9ed759600352256c7cb3
A. Joseph <pelimont34@outlook.com>
2019-09-10 14:35:49 -0400

    I fixed an issue with linking to script.js

--- script.js
+++ script.js
@@ -1,49 +1,84 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>the Vyne</title>
-    </head>
-    <body>
-        <form
-            onSubmit="postData(event)"
-            class="signupForm"
-        >
-            <input
-                type="text"
-                class="email"
-                placeholder="email"
-            />
-            <input
-                type="password"
-                class="password"
-                placeholder="password"
-            />
-            <input
-                type="text"
-                class="username"
-                placeholder="username"
-            />
-            <input type="submit" />
-        </form>
-        <form
-            onSubmit = "createPost(event)"
-            class="postForm"
-        >
-            <input
-                type="text"
-                class="title"
-                placeholder="title"
-            />
-            <input
-                type="text"
-                class="description"
-                placeholder="description"
-            />
-            <input type="submit" />
-        </form>
-        <ul class="posts">
-        </ul>
-        <script src="script.js"></script>
-    </body>
-</html>
+function postData(event) {
+    event.preventDefault();
+    const email = document.querySelector('.email');
+    const password = document.querySelector('.password');
+    const username = document.querySelector('.username');
+
+    fetch('http://thesi.generalassemb.ly:8080/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value,
+                username: username.value
+            })
+    })
+    .then((res) => {
+        return res.json();
+    })
+    .then((res) => {
+        localStorage.setItem('user', res.token);
+        createPost();
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+function updateDom() {
+    document.querySelector('.signupForm').style.display = "none";
+    document.querySelector('.postForm').style.display = "block";
+
+    fetch("http://thesi.generalassemb.ly:8080/user/post", {
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem('user')
+        }
+    })
+    .then((res) => {
+        return res.json();
+    })
+    .then((res) => {
+        const list = document.querySelector('.posts');
+
+        for (let i = 0; i < res.length; i++) {
+            const item = document.createElement('li');
+            const title = document.createElement('h3');
+            const description = document.createElement('p');
+            item.appendChild(title);
+            item.appendChild(description);
+            title.innerText = res[i].title;
+            description.innerText = res[i].description;
+            list.appendChild(item);
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+function createPost(event) {
+    event.preventDefault();
+    const title = document.querySelector('.title');
+    const description = document.querySelector('.description');
+
+    fetch("http://thesi.generalassemb.ly:8080/post", {
+        method: 'POST',
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem('user'),
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title.value,
+            description: description.value
+        })
+    })
+    .then((res) => {
+        console.log(res);
+        updateDom(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
