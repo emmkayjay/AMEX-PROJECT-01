16bfc5a6f8b9251227633ea1881d5eb8bc824b53
A. Joseph <pelimont34@outlook.com>
2019-09-10 20:48:48 -0400

    Created three user functions

--- js/script.js
+++ js/script.js
@@ -16,15 +16,20 @@ function postData(event) {
    })
    .then((res) => {
        return res.json();
+       console.log(res);
    })
+
    .then((res) => {
        localStorage.setItem('user', res.token);
-       createPost();
+       console.log(res)
+       //createPost();
    })
+
    .catch((err) => {
        console.log(err);
    })
 }
+
 function updateDom() {
    document.querySelector('.signupForm').style.display = "none";
    document.querySelector('.postForm').style.display = "block";
@@ -76,3 +81,28 @@ function createPost(event) {
        console.log(err);
    })
 }
+
+function createLogin(event) {
+   event.preventDefault();
+   const email = document.querySelector('.email');
+   const password = document.querySelector('.password');
+
+   fetch('http://thesi.generalassemb.ly:8080/login', {
+           method: 'POST',
+           headers: {
+               'Content-Type': 'application/json'
+           },
+           body: JSON.stringify({
+               email: email.value,
+               password: password.value
+           })
+   })
+   .then((res) => {
+       return res.json();
+       console(log);
+   })
+
+   .catch((err) => {
+       console.log(err);
+     })
+  }
